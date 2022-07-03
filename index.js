060acdf2453ee1b573e41c6efc3bd9f204f4f996
Camilla Poushyk <camilla.poushyk@gmail.com>
2022-07-03 11:26:42 -0400

    Done with asssignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(names, holiday) {
+    
+    let writtenCards = []
+    
+    for (let i = 0; i < names.length; i++) {
+
+        writtenCards[i] = `Thank you, ${names[i]}, for the wonderful ${holiday} gift!`;
+    }
+
+    return writtenCards;
+};
+
+function countDown(number) {
+
+    let i = 0;
+
+    while (i <= number) {
+
+        console.log(i++);
+    }
+}
